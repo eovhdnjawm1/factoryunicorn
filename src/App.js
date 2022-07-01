8d78629f1b4b28395cb36a7fa82c72723a203dc3
YoungJinGo <skyyj32@naver.com>
2022-07-01 23:00:52 +0900

    Feat Styled-components Setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
+const Title = styled.div`
+  font-size: 25px;
+  font-weight: bold;
+`
+
+
 function App() {
   return (
     <div className="App">
-
+      <Title>문의하시겠습니까?</Title>
     </div>
   );
 }
